7abb887111dbcd7e6385c3373a1f699a41611ac4
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-06-05 02:30:34 +0000

    test fix 7

--- src/badge/badge.controller.spec.ts
+++ src/badge/badge.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BadgeController } from './badge.controller';
 import { BadgeService } from './badge.service';
+import { Badge } from './entities/badge.entity';
+import { LeaderboardEntry } from 'src/leaderboard/entities/leaderboard.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('BadgeController', () => {
   let controller: BadgeController;
@@ -8,7 +11,25 @@ describe('BadgeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BadgeController],
-      providers: [BadgeService],
+      providers: [
+        BadgeService,
+        {
+          provide: getRepositoryToken(Badge),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(LeaderboardEntry),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BadgeController>(BadgeController);
--- src/badge/badge.service.spec.ts
+++ src/badge/badge.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BadgeService } from './badge.service';
-import { getRepository } from 'typeorm';
 import { Badge } from './entities/badge.entity';
+import { LeaderboardEntry } from 'src/leaderboard/entities/leaderboard.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('BadgeService', () => {
@@ -9,15 +9,25 @@ describe('BadgeService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BadgeService,
-      {
-        provide: getRepositoryToken(Badge),
-        useValue: {
-          find: jest.fn(),
-          findOne: jest.fn(),
-          save: jest.fn(),
+      providers: [
+        BadgeService,
+        {
+          provide: getRepositoryToken(Badge),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
         },
-      },      ]
+        {
+          provide: getRepositoryToken(LeaderboardEntry),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<BadgeService>(BadgeService);
