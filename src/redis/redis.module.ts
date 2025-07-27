0cecacbd8041d87e19abba497cc06b1454f8249d
gabito1451 <obiagaziegabriel@gmail.com>
2025-07-27 20:36:27 +0100

    Centralize Redis Client Usage and Add Caching in LeaderboardService

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Global, Module, OnModuleDestroy, Inject } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { redisProvider } from './redis.provider';
+import { REDIS_CLIENT } from './redis.constants';
+import Redis from 'ioredis';
+
+@Global()
+@Module({
+  imports: [ConfigModule],
+  providers: [redisProvider],
+  exports: [redisProvider],
+})
+export class RedisModule implements OnModuleDestroy {
+  constructor(@Inject(REDIS_CLIENT) private readonly redis: Redis) {}
+
+  async onModuleDestroy() {
+    if (this.redis) {
+      await this.redis.quit();
+      console.log('🧹 Redis disconnected gracefully');
+    }
+  }
+}
