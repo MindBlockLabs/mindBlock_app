0fd4b3244edce940be46858b329bc730880d6f15
ahmadogo <abdulkareemahmad413@gmail.com>
2026-01-24 16:49:29 +0100

    GET /puzzles – Filtered & Paginated

--- backend/src/puzzles/providers/puzzles.service.ts
+++ backend/src/puzzles/providers/puzzles.service.ts
@@ -2,14 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { CreatePuzzleProvider } from './create-puzzle.provider';
 import { CreatePuzzleDto } from '../dtos/create-puzzle.dto';
 import { Puzzle } from '../entities/puzzle.entity';
+import { PuzzleQueryDto } from '../dtos/puzzle-query.dto';
+import { InjectRepository } from '@nestjs/typeorm/dist/common/typeorm.decorators';
+import { Repository } from 'typeorm/repository/Repository';
+import { paginate } from 'src/common/pagination/paginate';
 
 @Injectable()
 export class PuzzlesService {
   constructor(
     private readonly createPuzzleProvider: CreatePuzzleProvider,
+
+    @InjectRepository(Puzzle)
+    private puzzleRepo: Repository<Puzzle>,
   ) {}
 
   public async create(createPuzzleDto: CreatePuzzleDto): Promise<Puzzle> {
     return this.createPuzzleProvider.execute(createPuzzleDto);
+  }
+
+  public async findAll(query: PuzzleQueryDto) {
+    const { categoryId, difficulty, page = 1, limit = 10 } = query;
+
+    const qb = this.puzzleRepo
+      .createQueryBuilder('puzzle')
+      .leftJoinAndSelect('puzzle.category', 'category')
+      .orderBy('puzzle.createdAt', 'DESC');
+
+    if (categoryId) {
+      qb.andWhere('puzzle.categoryId = :categoryId', { categoryId });
+    }
+
+    if (difficulty) {
+      qb.andWhere('puzzle.difficulty = :difficulty', { difficulty });
+    }
+
+    return paginate(qb, page, limit);
   }
 }
