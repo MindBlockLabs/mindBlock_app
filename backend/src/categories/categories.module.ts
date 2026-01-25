293072bcf780255e93ef299d0d6a96352bfc6ef4
phertyameen <aminubabafatima8@gmail.com>
2026-01-25 15:31:32 +0100

    readusted to nestjs/project best practices

--- backend/src/categories/categories.module.ts
+++ backend/src/categories/categories.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesController } from './categories.controller';
 import { Category } from './entities/category.entity';
+import { CategoriesService } from './providers/categories.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [],
-  exports: [],
+  providers: [CategoriesService],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}
