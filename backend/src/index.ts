fd9ec72d102feb77b7ec9dca908b79517b6c1dff
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2026-03-26 20:12:27 +0100

    Revert "Conditional middleware execution"

--- backend/src/index.ts
+++
@@ -1,3 +0,0 @@
-// Export conditional middleware utilities
-export { unless, onlyFor } from './common/middleware/utils/conditional.middleware';
-export type { RoutePattern } from './common/middleware/utils/conditional.middleware';
