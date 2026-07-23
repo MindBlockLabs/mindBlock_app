eff7db77a1dcda9d98c324344a8b07bb9e132afd
Anwanga-Abasi Frriday <brightidio377@gmail.com>
2026-07-23 13:45:09 +0100

    implemented nightly DAU rollup job

--- backend/src/analytics/analytics.module.ts
+++ backend/src/analytics/analytics.module.ts
@@ -2,21 +2,35 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalyticsEvent } from './entities/analytics-event.entity';
 import { RetentionCohort } from './entities/retention-cohort.entity';
+import { DailyActiveUser } from './entities/daily-active-user.entity';
 import { UsersAnalyticsListener } from './listeners/users-analytics.listener';
 import { AnalyticsController } from './controllers/analytics.controller';
 import { TrackEventProvider } from './providers/track-event.provider';
 import { GetOnboardingFunnelProvider } from './providers/get-onboarding-funnel.provider';
 import { GetRetentionCurveProvider } from './providers/get-retention-curve.provider';
+import { DailyActiveUsersRollupJob } from './jobs/daily-active-users-rollup.job';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnalyticsEvent, RetentionCohort])],
+  imports: [
+    TypeOrmModule.forFeature([
+      AnalyticsEvent,
+      RetentionCohort,
+      DailyActiveUser,
+    ]),
+  ],
   controllers: [AnalyticsController],
   providers: [
     UsersAnalyticsListener,
     TrackEventProvider,
     GetOnboardingFunnelProvider,
     GetRetentionCurveProvider,
+    DailyActiveUsersRollupJob,
   ],
-  exports: [TrackEventProvider, GetOnboardingFunnelProvider, GetRetentionCurveProvider, TypeOrmModule],
+  exports: [
+    TrackEventProvider,
+    GetOnboardingFunnelProvider,
+    GetRetentionCurveProvider,
+    TypeOrmModule,
+  ],
 })
 export class AnalyticsModule {}
